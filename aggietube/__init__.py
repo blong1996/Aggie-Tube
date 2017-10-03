0e35e4af3f122dfd7a7a1321438139a5a9962eee
blong1996 <blong1996@gmail.com>
2017-10-03 01:32:02 -0400

    implement YouTube API Phase I
    Allow for Search of videos and display top 20 videos

--- aggietube/__init__.py
+++ aggietube/__init__.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from youtube_tutorial.youtube import get_top_vids
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        videos = get_top_vids(request.form['query'])
+
+    else:
+        videos = get_top_vids('')
+
+    return render_template('index.html', videos=videos)
+
+
+@app.route('/<query>', methods=['POST'])
+def search(query):
+    videos = get_top_vids(query)
+
+    return render_template('index.html', videos=videos)
+
+
 if __name__ == '__main__':
     app.run()
