ba02163ec73d5be4ef37ff096dbaf357fc197ad6
blong1996 <blong1996@gmail.com>
2017-12-03 18:29:37 -0500

    Add tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 # Import flask and template operators
 from flask import Flask, render_template
-import testing
 
 # Import SQLAlchemy
 # from flask.ext.sqlalchemy import SQLAlchemy
@@ -10,7 +9,6 @@ app = Flask(__name__)
 
 # Configurations
 app.config.from_object('config')
-testing.test_main()
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.aggie_tube.controllers import aggie_tube as aggietube
